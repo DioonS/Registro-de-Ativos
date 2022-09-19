168afaa6a1996d6ae1f4b93e14f4884e03604c64
DioonS <itzdiih@gmail.com>
2022-09-19 12:40:50 -0300

    insert sql

---
+++ src/br/com/luft/dal/ModuloConexao.java
@@ -0,0 +1,25 @@
+package br.com.luft.dal;
+import java.sql.*;
+
+public class ModuloConexao {
+    // Metodo responsável por estabelecer a conexao com o banco
+    public static Connection conector() {
+        java.sql.Connection conexao = null;
+        // a linha a baixo "chama" o driver
+        String driver = "com.mysql.cj.jdbc.Driver";
+        // Armazenando informações referente ao banco
+        String url = "jdbc:mysql://localhost:3307/Portal";
+        String user = "root";
+        String password = "";
+        // Estabelecendo a conexão com o banco
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            // a linha abaixo serve de apoio para esclarecer o erro
+            //System.out.println(e);
+            return null;
+        }
+    }
+}
--- src/br/com/luft/window/WindowLogin.java
+++ src/br/com/luft/window/WindowLogin.java
@@ -1,22 +1,47 @@
 package br.com.luft.window;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import javax.swing.plaf.ColorUIResource;
-import javax.swing.plaf.metal.DefaultMetalTheme;
-import javax.swing.plaf.metal.MetalLookAndFeel;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
 
-public class WindowLogin extends JFrame{
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+import br.com.luft.dal.ModuloConexao;
+
+public class WindowLogin extends javax.swing.JFrame{
+
+    Connection conexao = null;
+    PreparedStatement pst = null;
+    ResultSet rs = null;
+
+    public void logar() {
+        String sql = "SELECT * FROM Usuario WHERE login = ? AND senha = ?";
+        try {
+            // Caputar o que está sendo digitado pelo usuario
+            // o ? é substitudo pelo conteudo informado
+            pst = conexao.prepareStatement(sql);
+            pst.setString(1, txtUsuario.getText());
+            pst.setString(2, txtSenha.getText());
+            // a linha a baixo executa a query
+            rs = pst.executeQuery();
+            // se existir usuario e senha correspondente
+            if (rs.next()) {
+                PrincipalWindow principal = new PrincipalWindow();
+
+                principal.setVisible(true);
+            } else {
+                JOptionPane.showMessageDialog(null, "Usuário e/ou Senha inválido!");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+
     private JButton btnCancel;
-    private JPasswordField fieldSenha;
-    private JTextField fieldUsuario;
+    private JPasswordField txtSenha;
+    private JTextField txtUsuario;
     private JPanel mainPanel;
     private JButton btnLogin;
     private JPanel panelImage;
+    private JLabel lblStatus;
 
     public WindowLogin() {
         setContentPane(mainPanel);
@@ -25,10 +50,22 @@ public class WindowLogin extends JFrame{
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
         setResizable(false);
-        ImageIcon image = new ImageIcon(getClass().getResource("luft logo.png"));
-        JLabel labelImage = new JLabel(image);
-        labelImage.add(labelImage);
-        labelImage.repaint();
+
+
+        // Permite conexao com o banoc de dados
+        conexao = ModuloConexao.conector();
+        // a linha abaixo serve de apoio ao status da conexao com o banco
+        //System.out.println(conexao);
+        if (conexao != null) {
+            lblStatus.setText("CONECTADO");
+            lblStatus.setForeground(Color.GREEN);
+            //lblStatus.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/luft/icons/dbyes.png")));
+
+        } else {
+            lblStatus.setText("DESCONECTADO");
+            lblStatus.setForeground(Color.RED);
+            //lblStatus.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/luft/icons/dbno.png")));
+        }
     }
 
 
